9d26bc0c188e4fa6e82da860f0d5cfcd6bf28bb6
makedevpro <makedevpro@gmail.com>
2020-08-10 16:13:40 +0300

    Added User Profile

--- a_male_2_bookmarks/settings.py
+++ a_male_2_bookmarks/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # указывает адрес, куда Django будет перенаправлять
 # пользователя при успешной авторизации, если не указан GET-параметр
